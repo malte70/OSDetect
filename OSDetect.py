12f9b3253b83fa0c6ffb58ff0fc97664a62e8ef9
Malte Bublitz <malte70+github@mcbx.de>
2016-06-01 21:19:24 +0200

    Added support for Msys

--- OSDetect.py
+++ OSDetect.py
@@ -4,28 +4,10 @@
 # OSDetect.py
 #    A simple solution to detect the operating system, including detection of wine
 #
-# Copyright © 2012-2013 Malte Bublitz, https://malte-bublitz.de
+# Copyright (c) 2012-2016 Malte Bublitz, https://malte70.github.io
 # All rights reserved.
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
 # 
-#  1. Redistributions of source code must retain the above copyright notice,
-#     this list of conditions and the following disclaimer.
-# 
-#  2. Redistributions in binary form must reproduce the above copyright notice,
-#     this list of conditions and the following disclaimer in the documentation
-#     and/or other materials provided with the distribution.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
-# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
-# EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# See COPYING.md for details.
 # 
 
 import sys
@@ -54,7 +36,7 @@ class OSInfo(object):
 			self.GetLinuxInfo()
 		elif sys.platform.startswith("freebsd"):
 			self.GetFreeBSDInfo()
-		elif sys.platform.startswith("win") or sys.platform.startswith("cygwin"):
+		elif sys.platform.startswith("win") or sys.platform.startswith("cygwin") or sys.platform.startswith("msys"):
 			self.GetWinNTInfo()
 		elif sys.platform.startswith("darwin"):
 			self.GetOSXInfo()
@@ -97,56 +79,69 @@ class OSInfo(object):
 		return {"implementation": platform.python_implementation(), "version": platform.python_version() }
 		
 	def GetWinNTUptime(self):
-		from uptime import uptime
-		seconds  = int(uptime())
-		days     = seconds/60/60/24
-		seconds -= days*60*60*24
-		hours    = seconds/60/60
-		seconds -= hours*60*60
-		minutes  = seconds/60
-		seconds -= minutes*60
-		up = {
-				"Days": days,
-				"Hours": hours,
-				"Minutes": minutes
-				}
-		up_str = u""
-		_DAYS = u" days"
-		_DAY = u" day"
-		_HOURS = u" hours"
-		_HOUR = u" hour"
-		_MINUTES = u" minutes"
-		_MINUTE = u" minute"
-		if up["Days"]      > 1:
-			up_str += ", " + unicode(up["Days"])    + _DAYS
-		elif up["Days"]    > 0:
-			up_str += ", " + unicode(up["Days"])    + _DAY
-		if up["Hours"]     > 1:
-			up_str += ", " + unicode(up["Hours"])   + _HOURS
-		elif up["Hours"]   > 0:
-			up_str += ", " + unicode(up["Hours"])   + _HOUR
-		if up["Minutes"]   > 1:
-			up_str += ", " + unicode(up["Minutes"]) + _MINUTES
-		elif up["Minutes"] > 0:
-			up_str += ", " + unicode(up["Minutes"]) + _MINUTE
-		return up_str.lstrip(", ")
+		try:
+			from uptime import uptime
+			seconds  = int(uptime())
+			days     = seconds/60/60/24
+			seconds -= days*60*60*24
+			hours    = seconds/60/60
+			seconds -= hours*60*60
+			minutes  = seconds/60
+			seconds -= minutes*60
+			up = {
+					"Days": days,
+					"Hours": hours,
+					"Minutes": minutes
+					}
+			up_str = u""
+			_DAYS = u" days"
+			_DAY = u" day"
+			_HOURS = u" hours"
+			_HOUR = u" hour"
+			_MINUTES = u" minutes"
+			_MINUTE = u" minute"
+			if up["Days"]      > 1:
+				up_str += ", " + unicode(up["Days"])    + _DAYS
+			elif up["Days"]    > 0:
+				up_str += ", " + unicode(up["Days"])    + _DAY
+			if up["Hours"]     > 1:
+				up_str += ", " + unicode(up["Hours"])   + _HOURS
+			elif up["Hours"]   > 0:
+				up_str += ", " + unicode(up["Hours"])   + _HOUR
+			if up["Minutes"]   > 1:
+				up_str += ", " + unicode(up["Minutes"]) + _MINUTES
+			elif up["Minutes"] > 0:
+				up_str += ", " + unicode(up["Minutes"]) + _MINUTE
+			return up_str.lstrip(", ")
+		except ImportError:
+			return "unknown"
 		
 	def GetWinNTProductInfo(self):
 		"""Get Information about the Windows NT Product,
 		a.k.a. the official Name like Windows 7"""
-		RegistryKey = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\Microsoft\Windows NT\CurrentVersion")
-		if not ISCYGWIN:
-			return _winreg.QueryValueEx(RegistryKey, "ProductName")+" "+_winreg.QueryValueEx(RegistryKey, "CSDVersion")
-		else:
-			return _winreg.QueryValueEx(RegistryKey, "ProductName")[0]+" "+_winreg.QueryValueEx(RegistryKey, "CSDVersion")[0]
+		try:
+			RegistryKey = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\Microsoft\Windows NT\CurrentVersion")
+			if not ISCYGWIN:
+				product = _winreg.QueryValueEx(RegistryKey, "ProductName")+" "+_winreg.QueryValueEx(RegistryKey, "CSDVersion")
+			else:
+				product = _winreg.QueryValueEx(RegistryKey, "ProductName")[0]+" "+_winreg.QueryValueEx(RegistryKey, "CSDVersion")[0]
+		except NameError: # _winreg doesn't exist on msys
+			product = platform.uname().system + " " + platform.uname().release
+			
+		return product
 		
 	def GetWinNTVersion(self):
 		"""Get the NT Version of an Windows System, e.g. 6.1 for Windows 7"""
-		RegistryKey = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\Microsoft\Windows NT\CurrentVersion")
-		if not ISCYGWIN:
-			return _winreg.QueryValueEx(RegistryKey, "CurrentVersion")
-		else:
-			return _winreg.QueryValueEx(RegistryKey, "CurrentVersion")[0]
+		try:
+			RegistryKey = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\Microsoft\Windows NT\CurrentVersion")
+			if not ISCYGWIN:
+				return _winreg.QueryValueEx(RegistryKey, "CurrentVersion")
+			else:
+				return _winreg.QueryValueEx(RegistryKey, "CurrentVersion")[0]
+		except NameError: # _winreg doesn't exist on msys
+			return ".".join(
+				platform.uname().version.split(".")[:2]
+			)
 		
 	def GetInfo(self):
 		"""Return the obtained information."""
